39cf7d10fd57c3d503c194e969240b9c2fe6ae8e
DmitryLebedev1992 <Lebedevds1992@mail.ru>
2025-05-29 14:14:24 +0500

    fix1

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
     static char[] actions = new char[]{'+', '-', '*', '/'};
     
     public static void main(String[] args) {
-        System.out.println("Введите два арабских или римских числа от 1 до 10 и арифметический оператор:");
+        System.out.println("Введите два арабских числа от 1 до 10 и арифметический оператор:");
         Scanner console = new Scanner(System.in);
         String inPut = console.nextLine();
         try {
@@ -56,8 +56,7 @@ public class Main {
         
         String type = definition(firstNumber, secondNumber);
         
-        if (type.equals("Arabic")) {
-            System.out.println("Вы ввели АРАБСКИЕ числа от 1 до 10.");
+        if (type.equals("ArabicFrom1to10")) {
                 int number1 = Integer.parseInt(firstNumber);
                 int number2 = Integer.parseInt(secondNumber);
                 
@@ -65,22 +64,8 @@ public class Main {
                 return String.valueOf(resultArabic);
         }
         
-        if(type.equals("Roman")) {
-            int number1 = searchIndexRoman(firstNumber);
-            int number2 = searchIndexRoman(secondNumber);
-            
-            int result = Result(number1, number2, regexAction);
-            if(result > 0){
-                System.out.println("Вы ввели РИМСКИЕ числа от 1 до 10.");
-                return convertNumToRoman(result);
-            } else {
-                throw new IllegalArgumentException("Результат при римских числах может быть только больше 0");
-            }
-        }
-        
-        throw new IllegalArgumentException("Введенные данные не являются или только АРАБСКИМИ или только РИМСКИМИ цифрами от 1 до 10");
+        throw new IllegalArgumentException("Введенные данные не являются АРАБСКИМИ цифрами от 1 до 10");
     }
-    
     
     public static int Result(int number1, int number2, String regexAction) {
         int result = 0;
@@ -108,48 +93,9 @@ public class Main {
         boolean matchArabicNumbersRight = SecondNumberArabic.matches();
         
         if (matchArabicNumbersLeft && matchArabicNumbersRight) {
-            definition = "Arabic";
+            definition = "ArabicFrom1to10";
             return definition;
         }
-        
-        String romanNumbers = "I|II|III|IV|V|VI|VII|VIII|IX|X";
-        Pattern patternRoman = Pattern.compile(romanNumbers);
-        Matcher FirstNumberRoman = patternRoman.matcher(firstNumber);
-        Matcher SecondNumberRoman = patternRoman.matcher(secondNumber);
-        boolean matchRomanNumbersLeft = FirstNumberRoman.matches();
-        boolean matchRomanNumbersRight = SecondNumberRoman.matches();
-        
-        if (matchRomanNumbersLeft && matchRomanNumbersRight) {
-            definition = "Roman";
-            return definition;
-        }
-        
         return definition;
-    }
-    
-    public static String convertNumToRoman(int arabicNumbers) {
-        String[] roman = {
-                "O", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX",
-                "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
-                "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX",
-                "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL",
-                "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX", "L",
-                "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX",
-                "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX",
-                "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX",
-                "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC",
-                "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C"
-        };
-        return roman[arabicNumbers];
-    }
-    
-    public static int searchIndexRoman(String RomanNumber) {
-        String[] roman = {"O", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
-        for (int i = 0; i < roman.length; i++) {
-            if (roman[i].equals(RomanNumber)) {
-                return i;
-            }
-        }
-        return -1;
     }
 }
